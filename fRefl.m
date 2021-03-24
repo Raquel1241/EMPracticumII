e3d9284529090b3f65433815ff57bcbeb48a07dc
VictorSchonk <victorschonk@hotmail.com>
2021-03-24 12:50:27 +0100

    initial work

---
+++ fRefl.m
@@ -0,0 +1,8 @@
+function [Gamma_H,Gamma_V] = fRefl(E1,E2,Theta_i)
+%FREFL Fresnel reflection coefficients. [Gamma_H, Gamma_V] from E1, E2 and
+%Theta_i
+%   Using the incident angle (Theta_i) and the permativities from the
+%   media. Assumed is that the dielectrics are nonmagnetic.
+	
+end
+
---
+++ pRatio.m
@@ -0,0 +1,19 @@
+function [p] = pRatio(theta,tau)
+%PRATIO Calculate the polarization ratio from theta and tau.
+%   The polarization ratio p is calculated from theta and tau. This ratio
+%   is to compute the E-field by setting the horizontal amplitude to 1 and
+%   the vertical amplitude to p. This vector is then multiplied by the
+%   Ah (horizontal amplitude) and e^(j(omega*t-k*r+delta_h)); or that
+%   vector is multiplied by Eh (the horizontal field) and
+%   e^(j(omega*t-k*r)).
+%	
+%	The function returns (-)Inf if theta is .5*pi+k*pi.
+%	Theta has to be between		-pi		and		pi
+%	Tau has to be between		-.25*pi and		.25*pi.
+r_val = [];				% Create the temporary storage of the return value
+	% Inf is aproximated by a significantly high value bu the tan()
+	% function.
+r_val = (tan(theta)+1i*tan(tau))/(1-1i*(tan(theta)*tan(tau)));
+p = r_val;
+
+end
